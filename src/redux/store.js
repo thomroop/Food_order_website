2fb8eea6587574ef5f00cfe6906f858163ddb4ab
thomroop <thomas.roopa@gmail.com>
2025-06-11 17:23:45 +0530

    changed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './authSlice'
+import authReducer from './authSlice';
 import productReducer from './productSlice';
 
 const store = configureStore({
